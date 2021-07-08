92a4fd184f01f0e8fe2ef1fa777ce58cdb978f9b
Pedro Rocha de Souza <pedro_souza@riseup.net>
2021-07-08 01:20:37 -0300

    configs for terraformls,bashls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,4 +2,8 @@ require("plugins")
 require("keymappings")
 require("settings")
 require("lualine-conf")
-vim.cmd('source ./lua/completion-conf.vim')
+require("compe-conf")
+
+require'lspconfig'.terraformls.setup{}
+require'lspconfig'.bashls.setup{}     
+-- vim.cmd('source ./lua/completion-conf.vim')
