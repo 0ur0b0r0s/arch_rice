869d605dc101c5766f0a698e07bc175ae3680c73
Pedro Rocha de Souza <pedro_souza@riseup.net>
2021-07-08 23:52:47 -0300

    neovim lua keymappings

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -1,7 +1,7 @@
 -- leader mapping
-
 vim.api.nvim_set_keymap("n", "<Space>", "<NOP>", { noremap = true, silent = true })
 vim.g.mapleader = ' '
+
 -- saving
 
 vim.api.nvim_set_keymap("n", "<c-w><c-q>", ":wq<CR>", { noremap = true, silent=true })
@@ -34,30 +34,30 @@ vim.api.nvim_set_keymap("n", ";", ":", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Leader>sw", ":set wrap<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Leader>snw", ":set wrap<CR>", { noremap = true })
 
--- terminal 
+-- terminal
 vim.api.nvim_set_keymap("n", "<Leader>tv", ":vne<CR>:terminal<CR>i", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>th", ":sp<CR>:terminal<CR>i", { noremap = true, silent = true })
 
--- substitute 
+-- substitute
 vim.api.nvim_set_keymap("n", "S", ":%s//g<Left><Left>", { noremap = true })
 
--- git 
+-- git
 vim.api.nvim_set_keymap("n", "<Leader>g", ":Git<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Leader>gP", ":Git push<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Leader>gp", ":Git pull<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Leader>gl", ":Git log<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>gc", ":GV<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>gv", ":GV<CR>", { noremap = true })
 
 -- signify
 vim.api.nvim_set_keymap("n", "<Leader>st", ":SignifyToggle<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>j", "<plug>(signify-next-hunk)", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>k", "<plug>(signify-prev-hunk)", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>j", ":call sy#jump#next_hunk(v:count1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>k", ":call sy#jump#prev_hunk(v:count1)<CR>", { noremap = true, silent = true })
 
 -- open/source lua config
 vim.api.nvim_set_keymap("n", "<c-e><c-v>", ":tabnew ~/.config/nvim/init.lua<CR>", { noremap = true, silent=true })
-vim.api.nvim_set_keymap("n", "<c-s><c-x>", ":source %<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<c-s><c-x>", ":source %<CR>", { noremap = true, silent = true })
 
--- packer 
+-- packer
 vim.api.nvim_set_keymap("n", "<c-p><c-i>", ":PackerInstall<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<c-p><c-d>", ":PackerClean<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<c-p><c-s>", ":PackerStatus<CR>", { noremap = true })
@@ -70,3 +70,7 @@ vim.api.nvim_set_keymap("n", "-", "<c-W>-", { noremap = true, silent=true })
 vim.api.nvim_set_keymap("v", "<C-y>", "\"+y", { noremap = true, silent=true })
 vim.api.nvim_set_keymap("n", "<c-p><c-p>", "\"+p", { silent=true })
 vim.api.nvim_set_keymap("n", "Y", "y+", { noremap = true, silent=true })
+
+-- sessions
+vim.api.nvim_set_keymap("n", "<Leader>sc", ":mksession! ~/.config/nvim/sessions/", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>ss", ":source ~/.config/nvim/sessions/", { noremap = true })
